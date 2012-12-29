4a9a1ad1c313c5ff94e16365c21a849e1e20e4d8
Ryan Johns <ryanjohns@gmail.com>
2012-12-29 02:46:23 -0800

    verification

--- app/assets/javascripts/user_data.js
+++ app/assets/javascripts/user_data.js
@@ -3,6 +3,6 @@ var userData = null;
 function UserData(data) {
   this.user_id = data['id'];
   this.email = data['email'];
-  this.verified = data['verified?'];
+  this.verified = data['verified_at?'];
   this.encrypted_data = data['encrypted_data'];
 };
--- app/assets/javascripts/verify.js
+++ app/assets/javascripts/verify.js
@@ -1,5 +1,29 @@
 function Verify() {};
 
 Verify.init = function() {
-  
+  $('#verification_email').html(userData.email);
+  $('#verification_field').val('');
 };
+
+$(function() {
+  $('#verification_form').bind('ajax:success', function(evt, data, status, xhr) {
+    userData.verified = true;
+    Util.chooseSection();
+  })
+  .bind('ajax:error', function(evt, xhr, status, error) {
+    var error = Util.extractErrors(xhr);
+    alert(error);
+  })
+  .bind('ajax:beforeSend', function(evt, xhr, settings) {
+    settings.url = settings.url + '/' + userData.user_id + '/verify';
+    var btn = $('#verification_btn');
+    btn.data('origText', btn.val());
+    btn.attr('disabled', 'disabled');
+    btn.val('Verifying...');
+  })
+  .bind('ajax:complete', function(evt, xhr, status) {
+    var btn = $('#verification_btn');
+    btn.val(btn.data('origText'));
+    btn.removeAttr('disabled');
+  });
+});
