5108d69c4002cc74c40f2ef483027e61808d0aab
Ryan Johns <ryanjohns@gmail.com>
2012-12-30 06:27:14 -0800

    compile assests at book in production unicorn listens on a port instead of a socket in production ignore .sass-cache

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,13 +15,7 @@ preload_app true
 FileUtils.mkdir_p("#{app_dir}/tmp/pids")
 FileUtils.mkdir_p("#{app_dir}/tmp/sockets")
 
-# listen on a Unix domain socket or a TCP port
-if ENV['RAILS_ENV'] == 'production'
-  listen("#{app_dir}/tmp/sockets/nginx.socket")
-else
-  listen('8080')
-end
-
+listen('8080')
 pid "#{app_dir}/tmp/pids/unicorn_#{Process.pid}.pid"
 
 stderr_path "#{app_dir}/log/unicorn-stderr.log"
