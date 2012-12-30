86c4054621cadecbce0247cbe4d1a43b4f95f03f
Ryan Johns <ryanjohns@gmail.com>
2012-12-29 19:35:09 -0800

    add method to display account list

--- app/assets/javascripts/accounts.js
+++ app/assets/javascripts/accounts.js
@@ -7,11 +7,27 @@ Accounts.init = function() {
 
 Accounts.selectView = function() {
   if (userData.masterPassword) {
+    this.fillAccountsTable();
     $('#unlock_accounts').hide();
     $('#accounts_list').show();
   } else {
     $('#accounts_list').hide();
     $('#unlock_accounts').show();
+  }
+};
+
+Accounts.fillAccountsTable = function() {
+  $('#accounts_table tr.account-data').remove();
+  for (account in userData.accounts) {
+    var row = $('#accounts_table tr.template').clone(true);
+    row.find('td.account').html(account);
+    row.find('td.username').html(userData.accounts[account]['username']);
+    row.find('a[data-password]').attr('data-password', userData.accounts[account]['password']);
+    row.find('td.notes').html(userData.accounts[account]['notes']);
+    row.removeClass('template');
+    row.addClass('account-data');
+    row.appendTo('#accounts_table tbody');
+    row.removeAttr('style');
   }
 };
 
@@ -34,6 +50,18 @@ $(function() {
       alert('Bad password.');
     else
       Accounts.unlock(passwd);
+    return false;
+  });
+
+  $('a[data-password]').click(function() {
+    var span = $(this).parent().find('span.password');
+    if (span.html() == '') {
+      span.html($(this).attr('data-password'));
+      $(this).html('hide');
+    } else {
+      span.html('');
+      $(this).html('show');
+    }
     return false;
   });
 });
