69d8920d323f6a779efebf95cf6283e330dbbefd
Ryan Johns <ryanjohns@gmail.com>
2013-01-03 01:32:53 -0800

    make offline mode actually work precompiling assets is now required

--- config/initializers/offline.rb
+++ config/initializers/offline.rb
@@ -1,5 +1,11 @@
-APPLICATION_MANIFEST = Rack::Offline.configure do
-  cache "#{Rails.configuration.assets[:prefix]}/application.js"
-  cache "#{Rails.configuration.assets[:prefix]}/application.css"
+APPLICATION_MANIFEST = Rack::Offline.configure({ :cache => Rails.env.production?, :root => Rails.public_path }) do
+  if Rails.env.production?
+    manifest = YAML.load_file("#{Rails.public_path}/assets/manifest.yml") rescue {}
+    cache "/assets/#{manifest['application.css']}"
+    cache "/assets/#{manifest['application.js']}"
+  else
+    cache '/assets/application.css'
+    cache '/assets/application.js'
+  end
   network '/'
 end
