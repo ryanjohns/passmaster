ae17e92916e2f68618764c0073e4cd4bbaebe57e
Ryan Johns <ryanjohns@gmail.com>
2013-01-07 18:51:49 -0800

    check if the server schema is newer than the application schema and force a page reload

--- app/assets/javascripts/schema.js
+++ app/assets/javascripts/schema.js
@@ -5,6 +5,11 @@ Schema.currentVersion = 1;
 Schema.migrate = function(version, data) {
   if (version == this.currentVersion)
     return data;
+  if (version > this.currentVersion) {
+    alert('You are using an outdated version of the website and need to refresh the page to continue.');
+    location.reload();
+    return;
+  }
   while (version < this.currentVersion) {
     version++;
     data = eval('Schema.migrate' + version + '(data)');
