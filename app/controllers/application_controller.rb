cbd9a85cec1113785e9f3a78a4b0c3c25a07883e
Ryan Johns <ryanjohns@gmail.com>
2013-01-26 00:54:45 -0800

    force_ssl in production mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :force_ssl, :except => [ :healthz ]
+
   def index
   end
 
@@ -9,6 +11,7 @@ class ApplicationController < ActionController::Base
       cookies.permanent.signed[:_client_id] = {
         :value    => UUIDTools::UUID.random_create.hexdigest,
         :httponly => true,
+        :secure   => Rails.configuration.force_ssl,
       }
     end
     render :json => { :token => form_authenticity_token }
@@ -25,6 +28,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def force_ssl
+    redirect_to :protocol => 'https' if Rails.configuration.force_ssl && !request.ssl?
+  end
 
   def respond_with_json(object)
     respond_with(object) do |format|
