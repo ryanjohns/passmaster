98491994ff9b6c20fbaa9ddd026c536fad849197
Ryan Johns <ryanjohns@gmail.com>
2013-01-30 15:59:07 -0800

    otp_sessions_controller tests

--- test/functional/otp_sessions_controller_test.rb
+++ test/functional/otp_sessions_controller_test.rb
@@ -2,7 +2,32 @@ require 'test_helper'
 
 class OtpSessionsControllerTest < ActionController::TestCase
 
-  test "create" do
+  test 'create with valid params' do
+    u = FactoryGirl.create(:user)
+    c = ROTP::TOTP.new(u.otp_secret).now
+    @request.cookie_jar.permanent.signed[:_client_id] = FactoryGirl.generate(:uuid)
+    assert_difference('OtpSession.count') do
+      post :create, { :user_id => u.id, :api_key => u.api_key, :otp => c }
+    end
+    assert_response :success
+  end
+
+  test 'create with invalid params' do
+    u = FactoryGirl.create(:user)
+    @request.cookie_jar.permanent.signed[:_client_id] = FactoryGirl.generate(:uuid)
+    assert_difference('OtpSession.count') do
+      post :create, { :user_id => u.id, :api_key => u.api_key, :otp => '123' }
+    end
+    assert_response :precondition_failed
+  end
+
+  test 'create when locked' do
+    o = FactoryGirl.create(:otp_session, :failed_count => OtpSession::MAX_FAILS)
+    @request.cookie_jar.permanent.signed[:_client_id] = o.client_id
+    assert_no_difference('OtpSession.count') do
+      post :create, { :user_id => o.user_id, :api_key => o.user.api_key, :otp => '123' }
+    end
+    assert_response :locked
   end
 
 end
