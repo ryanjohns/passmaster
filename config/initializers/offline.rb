12cf3427d21321a00433a738124952a84db05386
Ryan Johns <ryanjohns@gmail.com>
2013-02-01 15:53:14 -0800

    make spinner.gif an asset and change cache manifest for glyphicons

--- config/initializers/offline.rb
+++ config/initializers/offline.rb
@@ -1,19 +1,20 @@
-data = [
-  'CACHE MANIFEST',
-  '/img/glyphicons-halflings.png',
-  '/img/glyphicons-halflings-white.png',
-  '/img/spinner.gif',
-]
-
+data   = [ 'CACHE MANIFEST' ]
 assets = [
   'application.css',
   'application.js',
+  'spinner.gif',
+]
+other  = [
+  '/img/glyphicons-halflings.png',
+  '/img/glyphicons-halflings-white.png',
 ]
 if Rails.env.production?
   manifest = YAML.load_file("#{Rails.configuration.assets.manifest}/manifest.yml")
   data += assets.map { |asset| "#{ActionController::Base.asset_host}#{Rails.configuration.assets.prefix}/#{manifest[asset]}" }
+  data += other.map  { |other| "#{ActionController::Base.asset_host}#{other}" }
 else
   data += assets.map { |asset| "#{Rails.configuration.assets.prefix}/#{asset}" }
+  data += other
 end
 
 data += [ 'NETWORK:', '/' ]
