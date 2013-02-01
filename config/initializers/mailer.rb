9bcd05178509a747b69d9876a940e32c009a4394
Ryan Johns <ryanjohns@gmail.com>
2013-01-31 23:16:29 -0800

    send email via gmail until ses is up and running

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,6 +2,11 @@ ActionMailer::Base.default_url_options = {
   :host => 'passmaster.hoodquarters.com',
 }
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.comcast.net',
-  :port    => 25,
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  :domain               => 'passmaster.io',
+  :user_name            => 'no-reply@passmaster.io',
+  :password             => 'fhJQ4vOp5mDjXlb1LePDSS7zrOhikdq8',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true,
 }
