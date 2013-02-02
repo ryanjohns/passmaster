b97b696114a4def5dc83725a9ff8866934c9117e
Ryan Johns <ryanjohns@gmail.com>
2013-02-01 20:02:39 -0800

    Revert "make spinner.gif an asset and change cache manifest for glyphicons"
    This reverts commit 6bf1202509dbbb2b0206cf42b1eab4ad8924a507.

--- config/initializers/offline.rb
+++ config/initializers/offline.rb
@@ -1,20 +1,19 @@
-data   = [ 'CACHE MANIFEST' ]
+data = [
+  'CACHE MANIFEST',
+  '/img/glyphicons-halflings.png',
+  '/img/glyphicons-halflings-white.png',
+  '/img/spinner.gif',
+]
+
 assets = [
   'application.css',
   'application.js',
-  'spinner.gif',
-]
-other  = [
-  '/img/glyphicons-halflings.png',
-  '/img/glyphicons-halflings-white.png',
 ]
 if Rails.env.production?
   manifest = YAML.load_file("#{Rails.configuration.assets.manifest}/manifest.yml")
   data += assets.map { |asset| "#{ActionController::Base.asset_host}#{Rails.configuration.assets.prefix}/#{manifest[asset]}" }
-  data += other.map  { |other| "#{ActionController::Base.asset_host}#{other}" }
 else
   data += assets.map { |asset| "#{Rails.configuration.assets.prefix}/#{asset}" }
-  data += other
 end
 
 data += [ 'NETWORK:', '/' ]
