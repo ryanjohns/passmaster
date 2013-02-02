326df12ef199247695c1467ef3c103fcc6540a2a
Ryan Johns <ryanjohns@gmail.com>
2013-02-01 20:03:26 -0800

    Revert "remove rack-offline in favor of generating the cache manifest manually"
    This reverts commit f0a8db5711833b523d3bb0663cc4e8a84dadafdd.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Passmaster::Application.configure do
   config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
-  config.assets.manifest = "#{Rails.root}/config"
+  # config.assets.manifest = "#{Rails.root}/config"
 
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -46,7 +46,7 @@ Passmaster::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = 'https://passmaster.s3.amazonaws.com'
+  # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
