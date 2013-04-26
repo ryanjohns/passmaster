0630cb03473fa7f56ed929ba632cf393490a4873
Ryan Johns <ryanjohns@gmail.com>
2013-04-26 13:21:32 -0700

    add logic to prevent overwriting data when a remote update occurs. server-side check is commentted out for the roll out.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   before_filter :verify_user, :only => [ :update ]
   before_filter :verify_api_key, :only => [ :show, :update, :backup ]
   before_filter :verify_otp_session, :only => [ :show, :update, :backup ]
+  # before_filter :verify_version_code, :only => [ :update ]
 
   def show
     respond_with_json(@user)
@@ -75,6 +76,12 @@ class UsersController < ApplicationController
   def verify_otp_session
     unless @user.valid_otp_session?(cookies.signed[:_client_id], params[:otp_enabled], request.remote_ip, request.user_agent)
       render :json => { :errors => { :otp_session => ['has not been established'] } }, :status => :precondition_failed
+    end
+  end
+
+  def verify_version_code
+    unless @user.version_code == params[:version_code]
+      render :json => { :errors => { :version_code => ['does not match expected value'] } }, :status => :unprocessable_entity
     end
   end
 
