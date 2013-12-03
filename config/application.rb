55da51b7307e8d0882d327c95aa700b0c0c36148
Ryan Johns <ryanjohns@gmail.com>
2013-12-03 15:23:15 -0800

    update rails and a couple other gems

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Passmaster
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
+    I18n.enforce_available_locales = true
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
