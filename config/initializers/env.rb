3ac8df6eaafe9a534cd53613cde91f4037d08f27
Ryan Johns <ryanjohns@gmail.com>
2014-08-02 20:06:33 -0700

    add newrelic license to env

--- config/initializers/env.rb
+++ config/initializers/env.rb
@@ -1,10 +1,16 @@
+require 'yaml'
+
 env_file = File.expand_path('../../env.local.yml', __FILE__)
 env_file = File.expand_path('../../env.yml', __FILE__) unless File.file?(env_file)
 
 if File.file?(env_file)
-  YAML.load_file(env_file).each do |env, vars|
-    vars.each do |k, v|
-      ENV["#{env.upcase}_#{k.upcase}"] = v.to_s
+  YAML.load_file(env_file).each do |key, vars|
+    if vars.is_a?(Hash)
+      vars.each do |k, v|
+        ENV["#{key.upcase}_#{k.upcase}"] = v.to_s
+      end
+    else
+      ENV[key.upcase] = vars.to_s
     end
   end
 end
