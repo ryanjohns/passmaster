0849b73ed8600258ffdbff76e4b8b3ada4833353
Ryan Johns <ryanjohns@gmail.com>
2024-02-19 00:45:04 -0800

    update rails to 7.1.3

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '7.0.8'
+gem 'rails', '7.1.3'
 
 gem 'aws-sdk-rails'
 gem 'aws-sdk-s3'
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,12 @@ Bundler.require(*Rails.groups)
 module Passmaster
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -30,8 +35,7 @@ module Passmaster
     # in config/environments, which are processed later.
     #
     config.time_zone = "UTC"
-    config.eager_load_paths << Rails.root.join("lib")
-    config.active_support.cache_format_version = 7.0
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
